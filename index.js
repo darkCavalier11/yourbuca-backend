59d3dd53e8811e9a93beb05011cad77b183890fa
sumit <sumit.pradhan@gmail.com>
2020-12-13 21:15:59 +0530

    start script

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 // DB config
 const connectionUrl =
